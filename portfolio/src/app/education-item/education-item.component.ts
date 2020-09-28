c7b44bfbfb95add0b7954ff36d5c7034d9674644
Nicolas Macian <nicoomacian@gmail.com>
2020-09-28 19:45:09 -0300

    Education item update

--- portfolio/src/app/education-item/education-item.component.ts
+++ portfolio/src/app/education-item/education-item.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-education-item',
-  templateUrl: './education-item.component.html',
-  styleUrls: ['./education-item.component.scss']
+	selector: 'app-education-item',
+	templateUrl: './education-item.component.html',
+	styleUrls: ['./education-item.component.scss']
 })
 export class EducationItemComponent implements OnInit {
 
-  constructor() { }
+	@Input()
+	type: string;
 
-  ngOnInit(): void {
-  }
+	@Input()
+	title: string;
+
+	@Input()
+	subtitle1?: string;
+
+	@Input()
+	subtitle1_url?: string;
+
+	@Input()
+	subtitle2?: string;
+
+	@Input()
+	subtitle2_url?: string;
+
+	@Input()
+	organization: string;
+
+	@Input()
+	start: string;
+
+	@Input()
+	end?: string;
+
+	@Input()
+	description?: string;
+
+	@Input()
+	logo: string;
+
+	@Input()
+	logo_alt?: string;
+
+	@Input()
+	url?: string;
+
+	@Input()
+	courses?: object[];
+
+	listing: boolean = false;
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	openCourses(){
+		this.listing = !this.listing;
+	}
 
 }
