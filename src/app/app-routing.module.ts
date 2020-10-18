05f38a4800a26b82d975e720fac9a919c3581e17
Nicolas Macian <40004186+nmacianx@users.noreply.github.com>
2020-10-18 18:38:51 -0300

    Push development to production (#2)
    * Set up new angular copy
    
    * Styles update
    
    * Update on portfolio
    
    * Favicon, title and hidden items update
    
    * Work complete and started education
    
    * Education item update
    
    * Update on education and contact
    
    * Finished first design iteration
    
    * Scroll action added
    
    * Scroll time update
    
    * Finer details update
    
    * Style fixes
    
    * Update to site
    
    * Project build and refractoring

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from '../app/principal/principal.component';
+
+const routes: Routes = [
+  { path: '', component: PrincipalComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
