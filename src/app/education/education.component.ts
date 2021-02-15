49645051d38a1ee251173875f26c1a86d62f45d4
Nicolas Macian <nicoomacian@gmail.com>
2021-02-14 22:13:05 -0300

    feat: :sparkles: Updated information on site

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,151 +1,189 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-education',
-	templateUrl: './education.component.html',
-	styleUrls: ['./education.component.scss']
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  items: any[] = [
+    {
+      type: 'degree',
+      title: 'Computer Engineer',
+      subtitle1: 'Facultad de Ciencias Exactas y Tecnología',
+      subtitle1_url: 'https://www.facet.unt.edu.ar/',
+      subtitle2: 'Universidad Nacional de Tucumán',
+      subtitle2_url: 'http://www.unt.edu.ar/',
+      organization: 'Universidad Nacional de Tucumán',
+      start: 'March 2016',
+      end: 'Nov 2020',
+      description: [
+        'Ranked first student at Facultad de Ciencias Exactas y Tecnología (school including ten engineering degrees) for the 2020 year with an average of 9.35/10.',
+        'I developed a facial verification system on a Raspberry Pi 4 as my graduation project using Tiny YoloV3 and Keras.',
+      ],
+      logo: '/work/unt.png',
+      url:
+        'https://appserver.facet.unt.edu.ar/mesaentradas/index.php/resoluciones/mostrararchivo/40238/15166',
+    },
+  ];
+  specializations: any[] = [
+    {
+      type: 'specialization',
+      title: 'Applied Data Science with Python Specialization',
+      organization: 'by Univerity of Michigan',
+      start: 'Aug 2020',
+      logo: {
+        logo_sp: '/education/umich.png',
+        logo_sp_alt: 'University of Michigan',
+        background: '#00264b',
+        logo_cr: '/education/coursera.png',
+        logo_cr_alt: 'Coursera',
+      },
+      url: 'https://www.coursera.org/account/accomplishments/specialization/5K7E3B4W9UQT',
+      courses: [
+        {
+          title: 'Applied Social Network Analysis in Python',
+          organization: 'by Univerity of Michigan',
+          start: 'Aug 2020',
+          url: 'https://www.coursera.org/account/accomplishments/verify/E5N27UZ7PFNY',
+        },
+        {
+          title: 'Applied Text Mining in Python',
+          organization: 'by Univerity of Michigan',
+          start: 'May 2020',
+          url: 'https://www.coursera.org/account/accomplishments/verify/C5ASJ293YDNK',
+        },
+        {
+          title: 'Applied Machine Learning in Python',
+          organization: 'by Univerity of Michigan',
+          start: 'Mar 2020',
+          url: 'https://www.coursera.org/account/accomplishments/verify/QJBH3LP9RVBF',
+        },
+        {
+          title: 'Applied Plotting, Charting & Data Representation in Python',
+          organization: 'by Univerity of Michigan',
+          start: 'Oct 2019',
+          url: 'https://www.coursera.org/account/accomplishments/verify/VJMVSAGBKF2J',
+        },
+        {
+          title: 'Introduction to Data Science in Python',
+          organization: 'by Univerity of Michigan',
+          start: 'Sep 2019',
+          url: 'https://www.coursera.org/account/accomplishments/verify/HR5BZSWJHUNJ',
+        },
+      ],
+    },
+    {
+      type: 'specialization',
+      title: 'Deep Learning Specialization',
+      organization: 'by deeplearning.ai',
+      start: 'Jan 2020',
+      logo: {
+        logo_sp: '/education/deeplearning.png',
+        logo_sp_alt: 'deeplearning.ai',
+        logo_cr: '/education/coursera.png',
+        logo_cr_alt: 'Coursera',
+      },
+      url: 'https://www.coursera.org/account/accomplishments/specialization/FYBUEFSNUSWE',
+      courses: [
+        {
+          title: 'Sequence Models',
+          organization: 'by deeplearning.ai',
+          start: 'Jan 2020',
+          url: 'https://www.coursera.org/account/accomplishments/verify/S4BG28UL9BQ4',
+        },
+        {
+          title: 'Convolutional Neural Networks',
+          organization: 'by deeplearning.ai',
+          start: 'Nov 2019',
+          url: 'https://www.coursera.org/account/accomplishments/verify/GX2QK2PAYL7S',
+        },
+        {
+          title: 'Structuring Machine Learning Projects',
+          organization: 'by deeplearning.ai',
+          start: 'Oct 2019',
+          url: 'https://www.coursera.org/account/accomplishments/verify/ALT5AP679RQA',
+        },
+        {
+          title:
+            'Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization',
+          organization: 'by deeplearning.ai',
+          start: 'Sep 2019',
+          url: 'https://www.coursera.org/account/accomplishments/verify/7XUJKNQ6VFUK',
+        },
+        {
+          title: 'Neural Networks and Deep Learning',
+          organization: 'by deeplearning.ai',
+          start: 'Aug 2019',
+          url: 'https://www.coursera.org/account/accomplishments/verify/EFR5P595JHU4',
+        },
+      ],
+    },
+    {
+      type: 'course',
+      title: 'Essential Cloud Infrastructure: Core Services',
+      organization: 'by Google',
+      start: 'Dec 2020',
+      logo: {
+        logo_sp: '/education/google.png',
+        logo_sp_alt: 'Google',
+      },
+      url: 'https://www.coursera.org/account/accomplishments/verify/MSWW86BR6BXS',
+      courses: [],
+    },
+    {
+      type: 'course',
+      title: 'TinyML1: Fundamentals of TinyML',
+      organization: 'by HarvardX',
+      start: 'Dec 2020',
+      logo: {
+        logo_sp: '/education/harvardx.png',
+        logo_sp_alt: 'HarvardX',
+      },
+      url: 'https://courses.edx.org/certificates/2423c8d7265e44ab925046f4a3e0a8c0',
+      courses: [],
+    },
+    {
+      type: 'course',
+      title: 'Essential Cloud Infrastructure: Foundation',
+      organization: 'by Google',
+      start: 'Nov 2020',
+      logo: {
+        logo_sp: '/education/google.png',
+        logo_sp_alt: 'Google',
+      },
+      url: 'https://www.coursera.org/account/accomplishments/verify/NG5P3EF3YL2R',
+      courses: [],
+    },
+    {
+      type: 'course',
+      title: 'Google Cloud Platform Fundamentals: Core Infrastructure',
+      organization: 'by Google',
+      start: 'Oct 2020',
+      logo: {
+        logo_sp: '/education/google.png',
+        logo_sp_alt: 'Google',
+      },
+      url: 'https://www.coursera.org/account/accomplishments/verify/ZVGNRZZWF29B',
+      courses: [],
+    },
+    {
+      type: 'course',
+      title: "Blockchain's Disruption",
+      organization: 'by Universidad Austral',
+      start: 'Oct 2020',
+      logo: {
+        logo_sp: '/education/austral.png',
+        logo_sp_alt: 'Universidad Austral',
+        background: '#103692',
+      },
+      url: 'https://www.coursera.org/account/accomplishments/verify/R6LF969GXDJG',
+      courses: [],
+    },
+  ];
+  courses: any[] = [];
 
-	items: any[] = [
-		{
-			type: 'degree',
-			title: 'Computer Engineer',
-			subtitle1: 'Facultad de Ciencias Exactas y Tecnología',
-			subtitle1_url: 'https://www.facet.unt.edu.ar/',
-			subtitle2: 'Universidad Nacional de Tucumán',
-			subtitle2_url: 'http://www.unt.edu.ar/',
-			organization: 'Universidad Nacional de Tucumán',
-			start: 'March 2016',
-			end: 'Nov 2020',
-			description: ["Ranked first student at Facultad de Ciencias Exactas y Tecnología (school including ten engineering degrees) for the 2020 year with an average of 9.35/10.", 'I developed a facial verification system on a Raspberry Pi 4 as my graduation project using Tiny YoloV3 and Keras.'],
-			logo: '/work/unt.png',
-			url: 'https://appserver.facet.unt.edu.ar/mesaentradas/index.php/resoluciones/mostrararchivo/40238/15166'
-		}
-	];
-	specializations: any[] = [
-		{
-			type: 'specialization',
-			title: 'Applied Data Science with Python Specialization',
-			organization: 'by Univerity of Michigan',
-			start: 'Aug 2020',
-			logo: {
-				logo_sp: '/education/umich.png',
-				logo_sp_alt: 'University of Michigan',
-				background: '#00264b',
-				logo_cr: '/education/coursera.png',
-				logo_cr_alt: 'Coursera',
-			},
-			url: 'https://www.coursera.org/account/accomplishments/specialization/5K7E3B4W9UQT',
-			courses: [
-				{
-					title: 'Applied Social Network Analysis in Python',
-					organization: 'by Univerity of Michigan',
-					start: 'Aug 2020',
-					url: 'https://www.coursera.org/account/accomplishments/verify/E5N27UZ7PFNY'
-				},
-				{
-					title: 'Applied Text Mining in Python',
-					organization: 'by Univerity of Michigan',
-					start: 'May 2020',
-					url: 'https://www.coursera.org/account/accomplishments/verify/C5ASJ293YDNK'
-				},
-				{
-					title: 'Applied Machine Learning in Python',
-					organization: 'by Univerity of Michigan',
-					start: 'Mar 2020',
-					url: 'https://www.coursera.org/account/accomplishments/verify/QJBH3LP9RVBF'
-				},
-				{
-					title: 'Applied Plotting, Charting & Data Representation in Python',
-					organization: 'by Univerity of Michigan',
-					start: 'Oct 2019',
-					url: 'https://www.coursera.org/account/accomplishments/verify/VJMVSAGBKF2J'
-				},
-				{
-					title: 'Introduction to Data Science in Python',
-					organization: 'by Univerity of Michigan',
-					start: 'Sep 2019',
-					url: 'https://www.coursera.org/account/accomplishments/verify/HR5BZSWJHUNJ'
-				}
-			]
-		},
-		{
-			type: 'specialization',
-			title: 'Deep Learning Specialization',
-			organization: 'by deeplearning.ai',
-			start: 'Jan 2020',
-			logo: {
-				logo_sp: '/education/deeplearning.png',
-				logo_sp_alt: 'deeplearning.ai',
-				logo_cr: '/education/coursera.png',
-				logo_cr_alt: 'Coursera',
-			},
-			url: 'https://www.coursera.org/account/accomplishments/specialization/FYBUEFSNUSWE',
-			courses: [
-				{
-					title: 'Sequence Models',
-					organization: 'by deeplearning.ai',
-					start: 'Jan 2020',
-					url: 'https://www.coursera.org/account/accomplishments/verify/S4BG28UL9BQ4'
-				},
-				{
-					title: 'Convolutional Neural Networks',
-					organization: 'by deeplearning.ai',
-					start: 'Nov 2019',
-					url: 'https://www.coursera.org/account/accomplishments/verify/GX2QK2PAYL7S'
-				},
-				{
-					title: 'Structuring Machine Learning Projects',
-					organization: 'by deeplearning.ai',
-					start: 'Oct 2019',
-					url: 'https://www.coursera.org/account/accomplishments/verify/ALT5AP679RQA'
-				},
-				{
-					title: 'Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization',
-					organization: 'by deeplearning.ai',
-					start: 'Sep 2019',
-					url: 'https://www.coursera.org/account/accomplishments/verify/7XUJKNQ6VFUK'
-				},
-				{
-					title: 'Neural Networks and Deep Learning',
-					organization: 'by deeplearning.ai',
-					start: 'Aug 2019',
-					url: 'https://www.coursera.org/account/accomplishments/verify/EFR5P595JHU4'
-				},
-			]
-		},
-		{
-			type: 'course',
-			title: 'Google Cloud Platform Fundamentals: Core Infrastructure',
-			organization: 'by Google',
-			start: 'Oct 2020',
-			logo: {
-				logo_sp: '/education/google.png',
-				logo_sp_alt: 'Google',
-			},
-			url: 'https://www.coursera.org/account/accomplishments/verify/ZVGNRZZWF29B',
-			courses: []
-		},
-		{
-			type: 'course',
-			title: 'Blockchain\'s Disruption',
-			organization: 'by Universidad Austral',
-			start: 'Oct 2020',
-			logo: {
-				logo_sp: '/education/austral.png',
-				logo_sp_alt: 'Universidad Austral',
-				background: '#103692',
-			},
-			url: 'https://www.coursera.org/account/accomplishments/verify/R6LF969GXDJG',
-			courses: []
-		},
-	];
-	courses: any[] = [];
+  constructor() {}
 
-	constructor() { }
-
-	ngOnInit(): void {
-	}
-
+  ngOnInit(): void {}
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -5,150 +5,169 @@ import { PageScrollService } from 'ngx-page-scroll-core';
 import * as AOS from 'aos';
 
 @Component({
-	selector: 'app-portfolio',
-	templateUrl: './portfolio.component.html',
-	styleUrls: ['./portfolio.component.scss']
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
+  more: boolean = true;
+  showing: number;
+  total: number;
+  add: number;
+  display: any[] = [];
+  items: any[] = [
+    {
+      title: 'Edumovil',
+      text: "School's communication and administration system.",
+      preview: 'preview/portfolio00.jpg',
+      tech: ['Django', 'Ionic', 'Angular'],
+      state: 'online',
+      url: 'https://usuarios.edumovil.com.ar/',
+      more: {
+        description: "School's communication and administration system.",
+        img: ['Edumovil00.jpg', 'Edumovil01.jpg', 'Edumovil02.jpg', 'Edumovil03.jpg'],
+      },
+    },
+    {
+      title: "Men in Kilts Valentine's Day",
+      text: "Valentine's Day postcard generation wizard site to send to your loved ones.",
+      preview: 'preview/portfolio14.png',
+      tech: ['Django', 'Javascript'],
+      state: 'online',
+      url: 'https://www.meninkiltsvalentines.com',
+    },
+    {
+      title: 'Treasure Cave Chili',
+      text: 'Recipe submission site to participate in sweepstakes including image framing wizard.',
+      preview: 'preview/portfolio13.png',
+      tech: ['Django', 'Javascript'],
+      state: 'online',
+      url: 'https://www.treasurecavechilicookoff.com/',
+    },
+    {
+      title: 'Lifetime Civic',
+      text: 'Form submission site to register employees for a company.',
+      preview: 'preview/portfolio12.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'private',
+    },
+    {
+      title: 'A+E Eats',
+      text: 'Form submission site for food ordering.',
+      preview: 'preview/portfolio05.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'private',
+    },
+    {
+      title: 'Stop&Shop Summer',
+      text: 'Promotion site.',
+      preview: 'preview/portfolio07.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'online',
+      url: 'http://www.stopandshopsummer.com/',
+    },
+    {
+      title: 'Caylee Hammack',
+      text: 'Sweepstakes site with image creation wizard.',
+      preview: 'preview/portfolio06.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'online',
+      url: 'https://www.ifitwasntforyousweeps.com/',
+    },
+    {
+      title: 'First Horizon Park',
+      text: "Website and postcards' design update for previously developed site.",
+      preview: 'preview/portfolio02.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'online',
+      url: 'https://www.firsthorizonparkpostcard.com/',
+    },
+    {
+      title: 'Day Owl Rose',
+      text: 'Sweepstakes site.',
+      preview: 'preview/portfolio03.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'offline',
+    },
+    {
+      title: 'Z. Alexander Brown',
+      text: 'Sweepstakes site.',
+      preview: 'preview/portfolio01.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'offline',
+    },
+    {
+      title: 'First Horizon Park',
+      text: "Holidays' postcards creation wizard site.",
+      preview: 'preview/portfolio04.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'online',
+      url: 'https://www.firsthorizonparkpostcard.com/',
+    },
+    {
+      title: "Hill's Pet Academy",
+      text: "Demo site for Hill's Pet Academy.",
+      preview: 'preview/portfolio08.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'offline',
+    },
+    {
+      title: 'prAna',
+      text: "'Dream Job' Promotion site.",
+      preview: 'preview/portfolio09.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'offline',
+    },
+    {
+      title: 'Robert Hall',
+      text: 'Sweepstakes site.',
+      preview: 'preview/portfolio10.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'offline',
+    },
+    {
+      title: 'Duckhorn',
+      text: 'Sweepstakes site.',
+      preview: 'preview/portfolio11.jpg',
+      tech: ['Django', 'Javascript'],
+      state: 'offline',
+    },
+  ];
 
-	more: boolean = true;
-	showing: number;
-	total: number;
-	add: number;
-	display: any[] = [];
-	items: any[] = [
-		{
-			title: "Edumovil",
-			text: "School's communication and administration system.",
-			preview: "preview/portfolio00.jpg",
-			tech: ["Django", "Ionic", "Angular"],
-			state: "online",
-			url: "https://usuarios.edumovil.com.ar/",
-			more: {
-				description: "School's communication and administration system.",
-				img: ['Edumovil00.jpg', 'Edumovil01.jpg', 'Edumovil02.jpg', 'Edumovil03.jpg']
-			}
-		}, 
-		{
-			title: "Lifetime Civic",
-			text: "Form submission site to register employees for a company.",
-			preview: "preview/portfolio12.jpg",
-			tech: ["Django", "Javascript"],
-			state: "private"
-		}, 
-		{
-			title: "A+E Eats",
-			text: "Form submission site for food ordering.",
-			preview: "preview/portfolio05.jpg",
-			tech: ["Django", "Javascript"],
-			state: "private"
-		}, 
-		{
-			title: "Stop&Shop Summer",
-			text: "Promotion site.",
-			preview: "preview/portfolio07.jpg",
-			tech: ["Django", "Javascript"],
-			state: "online",
-			url: "http://www.stopandshopsummer.com/"
-		}, 
-		{
-			title: "Caylee Hammack",
-			text: "Sweepstakes site with image creation wizard.",
-			preview: "preview/portfolio06.jpg",
-			tech: ["Django", "Javascript"],
-			state: "online",
-			url: "https://www.ifitwasntforyousweeps.com/"
-		}, 
-		{
-			title: "First Horizon Park",
-			text: "Website and postcards' design update for previously developed site.",
-			preview: "preview/portfolio02.jpg",
-			tech: ["Django", "Javascript"],
-			state: "online",
-			url: "https://www.firsthorizonparkpostcard.com/"
-		}, 
-		{
-			title: "Day Owl Rose",
-			text: "Sweepstakes site.",
-			preview: "preview/portfolio03.jpg",
-			tech: ["Django", "Javascript"],
-			state: "offline"
-		}, 
-		{
-			title: "Z. Alexander Brown",
-			text: "Sweepstakes site.",
-			preview: "preview/portfolio01.jpg",
-			tech: ["Django", "Javascript"],
-			state: "offline"
-		}, 
-		{
-			title: "First Horizon Park",
-			text: "Holidays' postcards creation wizard site.",
-			preview: "preview/portfolio04.jpg",
-			tech: ["Django", "Javascript"],
-			state: "online",
-			url: "https://www.firsthorizonparkpostcard.com/"
-		}, 
-		{
-			title: "Hill's Pet Academy",
-			text: "Demo site for Hill's Pet Academy.",
-			preview: "preview/portfolio08.jpg",
-			tech: ["Django", "Javascript"],
-			state: "offline"
-		}, 
-		{
-			title: "prAna",
-			text: "'Dream Job' Promotion site.",
-			preview: "preview/portfolio09.jpg",
-			tech: ["Django", "Javascript"],
-			state: "offline"
-		}, 
-		{
-			title: "Robert Hall",
-			text: "Sweepstakes site.",
-			preview: "preview/portfolio10.jpg",
-			tech: ["Django", "Javascript"],
-			state: "offline"
-		}, 
-		{
-			title: "Duckhorn",
-			text: "Sweepstakes site.",
-			preview: "preview/portfolio11.jpg",
-			tech: ["Django", "Javascript"],
-			state: "offline"
-		}, 
-	];
+  constructor(
+    public media: MediaService,
+    private pageScrollService: PageScrollService,
+    @Inject(DOCUMENT) private document: any,
+  ) {}
 
-	constructor(public media: MediaService, private pageScrollService: PageScrollService, @Inject(DOCUMENT) private document: any) { }
+  ngOnInit(): void {
+    if (this.media.breakpointSm.matches) {
+      this.showing = 3;
+      this.add = 3;
+    } else if (this.media.breakpointMd.matches) {
+      this.showing = 4;
+      this.add = 2;
+    } else {
+      this.showing = 6;
+      this.add = 3;
+    }
+    this.display = this.items.slice(0, this.showing);
+    this.total = this.items.length;
+  }
 
-	ngOnInit(): void {
-		if (this.media.breakpointSm.matches){
-			this.showing = 3;			
-			this.add = 3;
-		}else if (this.media.breakpointMd.matches){
-			this.showing = 4;
-			this.add = 2;
-		}else {
-			this.showing = 6;
-			this.add = 3;
-		}
-		this.display = this.items.slice(0, this.showing);
-		this.total = this.items.length;
-	}
-
-	showMore(event){
-		if (this.showing + this.add >= this.total){
-			this.showing = this.total;
-			this.more = false;
-		}else{
-			this.showing += this.add;
-		}
-		this.display = this.items.slice(0, this.showing);
-		this.pageScrollService.scroll({
-			document: this.document,
-			scrollTarget: event.srcElement,
-			scrollOffset: 200,
-			duration: 300
-		});
-	}
+  showMore(event) {
+    if (this.showing + this.add >= this.total) {
+      this.showing = this.total;
+      this.more = false;
+    } else {
+      this.showing += this.add;
+    }
+    this.display = this.items.slice(0, this.showing);
+    this.pageScrollService.scroll({
+      document: this.document,
+      scrollTarget: event.srcElement,
+      scrollOffset: 200,
+      duration: 300,
+    });
+  }
 }
